3787494f8c6f5a52ef12b80819eb10a00dbb3e3c
Vanessa Lago Machado <47528313+vanessalagomachado@users.noreply.github.com>
2025-05-15 21:28:00 -0300

    Aula 14 de maio

---
+++ src/modelo/Plano.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package modelo;
+
+/**
+ *
+ * @author vanessalagomachado
+ */
+public class Plano {
+    private String nome;
+    private String descricao;
+    private double valor;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+    
+    
+}
